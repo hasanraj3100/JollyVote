da100f54c197fa02c5c5c4c8a5c476435d470920
Raj <hasanraj3100@gmail.com>
2024-12-07 21:38:27 +0600

    fix: No page title for search result page

--- resources/js/Pages/Polls/SearchResult.jsx
+++ resources/js/Pages/Polls/SearchResult.jsx
@@ -1,11 +1,13 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.jsx";
 import Poll_forNewsFeed from "@/Pages/Polls/Partials/Poll_For_NewsFeed.jsx";
+import {Head} from "@inertiajs/react";
 
 export default function SearchResult({polls, query}) {
     const count = polls.length;
     console.log(polls);
     return (
         <AuthenticatedLayout>
+            <Head title={"Search Result"}/>
             <div className={'bg-white shadow-md p-6'}>
                 <h1 className={'font-bold text-xl'}>{count} results found for "{query}": </h1>
             </div>
